6589cd6ba7f66844a31d54804bcaec1d8e0cc71d
sinkyoungdeok <tlsrudejr123@nate.com>
2021-05-26 23:54:13 +0900

    [ADD] 간단한 api

--- ai/views.py
+++ ai/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+import requests
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    res = requests.get('http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey=hopxVnTUPGecxaK%2BhbSb0LK0%2BMonhKp8GTyVMtrD8DE2NSucFjfWCHja06wJuxKdE7eW59gNaSnjQcMiJ0Kd5g%3D%3D&numOfRows=10&pageNo=1&MobileOS=AND&MobileApp=appName&_type=json')
+    return HttpResponse(str(res.status_code) + " | " + res.text)
