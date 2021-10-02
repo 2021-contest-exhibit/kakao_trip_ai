cf3c3c71abff31a42dd8124cc32488aff5d1148d
sinkyoungdeok <tlsrudejr123@nate.com>
2021-10-02 17:04:04 +0900

    [ADD] REST API에 실제 AI 동작 추가 완성

--- modak_server/base.py
+++ modak_server/base.py
@@ -1,11 +1,26 @@
 import requests
 
 base_url = "http://localhost:8080/opendata/base"
+class Base(object):
+    def __init__(self, my_dict):
+        for key in my_dict:
+            setattr(self, key, my_dict[key])
+    def __str__ (self):
+        return '({}, {})'.format(self.contentId, self.intro)
+
 
 def create_base_data():
     response = requests.post(base_url)
     return response
 
 def get_base_data():
-    response = requests.get(base_url)
-    return response.json()
+    base_list = []
+    intro_list = []
+    json_data = requests.get(base_url + "/intro").json()
+    for j in json_data:
+        base = Base(j)
+
+        intro_list.append(j.get('intro'))
+        base_list.append(base)
+
+    return base_list, intro_list
