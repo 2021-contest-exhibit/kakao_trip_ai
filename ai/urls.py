bc3eda1efd9aa14e4eb048048b8b7b0792f63fe1
sinkyoungdeok <tlsrudejr123@nate.com>
2021-10-27 22:53:16 +0900

    [FIX] cmaping embedding을 서버에 저장하는 방식에서 json저장방식으로 변경

--- ai/urls.py
+++ ai/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index),
     path('embedding/all', views.all_camping_to_embedding_json),
     path('patch/camping/embedding', views.patch_campings_embedding),
+    path('recommend', views.recommend),
 ]
